e3dae363f911f0bc2074154a99c31fd21f9c10a8
Chistyakov Dmitriy <ds.chistyakov@jet.msk.su>
2016-09-02 16:59:57 +0400

    1

---
+++ Assets/Scripts/Tile/TileItemSameColorCount.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TileItemSameColorCount {
+	public TileItemTypeGroup TypeGroup;
+	
+	private int itemCount = 0;
+	private IDictionary<Vector2, Object> enevlopReplacePositions = new Dictionary<Vector2, Object>();
+
+	public TileItemSameColorCount(TileItemTypeGroup typeGroup) {
+		TypeGroup = typeGroup;
+	}
+
+	public int Increment() {
+		++itemCount;
+		return Count();
+	}
+
+	public int AddPositions(IList<TileItemData> positions) {
+		if(positions != null) {	
+			foreach(TileItemData item in positions) {
+				Vector2 pos = new Vector2(item.X, item.Y);
+				if(!enevlopReplacePositions.ContainsKey(pos)) {
+					enevlopReplacePositions.Add(pos, null);
+				}
+			}
+		}
+
+		return Count();
+	}
+
+	public int Count() {
+		return itemCount + enevlopReplacePositions.Count; 
+	}
+
+	public int GetItemCount() {
+		return itemCount;
+	}
+}
